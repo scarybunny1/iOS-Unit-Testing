6ba96317880c643025c06985e2c70599d129e8ae
scarybunny1 <46290395+scarybunny1@users.noreply.github.com>
2023-01-25 15:14:41 +0530

    Initial Setup

---
+++ AssertYourself/AssertYourself/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AssertYourself
+//
+//  Created by Ayush Bhatt on 25/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
